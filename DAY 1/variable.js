e005cf6a8023ea24fe289422972bcb6982209a44
MONIS1234 <2211cs010502@mallareddyuniversity.ac.in>
2025-01-09 10:25:05 +0530

    functtions

---
+++ DAY 1/variable.js
@@ -0,0 +1,21 @@
+var a=10
+console.log(a)
+
+let b=20
+console.log(b)
+
+const c=30
+console.log(c)
+// block scope
+{
+    var m=100
+    console.log(m)
+
+    let n=200
+    console.log(n)
+
+    const o=300
+   console.log(o)
+ 
+}
+console.log("end")
