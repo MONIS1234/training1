aae5996bc62caf14c938a472ff1303792b7ab185
MONIS1234 <2211cs010502@mallareddyuniversity.ac.in>
2025-01-22 15:18:49 +0530

    promise

--- DAY 5/async.js
+++ DAY 5/async.js
@@ -12,3 +12,33 @@
 
 
 //! Promise
+// let p1=new Promise((resolve,reject)=>{});
+// console.log(p1);
+
+// let p2=new Promise((resolve,reject)=>{
+//     resolve("Success");
+// })
+// console.log(p2);
+// p2.then((res) => {
+//     console.log(res);
+// }).catch(err=>console.log(err))
+// .finally(()=>console.log("final"))
+
+
+// let p3 =new Promise((resolve,reject)=>{
+//     reject("Failures");
+// });
+
+// p3.then(res=>console.log(res))
+// .catch(err=>console.log(err))
+// .finally(()=>console.log("final"))
+function fetchUsers(){
+    let response = fetch("https://jsonplaceholder.typicode.com/users");
+    response.then(res =>{
+        return res.json().then(data =>{
+            console.log(data);
+        })
+    })
+    .catch(err=>console.log(err))
+}
+fetchUsers();
