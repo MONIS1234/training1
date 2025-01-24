c0e53085927f4136f7a4380ca0dd611501176914
MONIS1234 <2211cs010502@mallareddyuniversity.ac.in>
2025-01-24 11:39:51 +0530

    dom

---
+++ dom.js
@@ -0,0 +1,23 @@
+// let ele =document.getElementsByClassName("test");
+// ele[0].style.backgroundColor="dodgerblue"
+//console.log(ele);
+//console.log(Array.isArray(ele));
+
+//spread opearator
+
+
+
+// let x=[...ele];
+// console.log(x,Array.isArray(x));
+// x.map(element =>{
+//     element.style.backgroundColor="tomato";
+//     element.style.color="white";
+//     element.style.textTransform="uppercase";
+// })
+ let ele =document.querySelectorAll(".test");
+console.log(ele);
+
+// let ele =document.querySelector("#demo");
+// console.log(ele);
+// let ele=document.querySelector(".test");
+// console.log(ele);
