c8199ce6476d0b099ed7a6390a0a7bacdbfc2af3
MONIS1234 <2211cs010502@mallareddyuniversity.ac.in>
2025-01-29 10:42:07 +0530

    DOM

--- dom.js
+++ dom.js
@@ -1,23 +1,121 @@
-// let ele =document.getElementsByClassName("test");
-// ele[0].style.backgroundColor="dodgerblue"
-//console.log(ele);
-//console.log(Array.isArray(ele));
+// let ele = document.createElement("h1");
+// ele.innerText= "dynamic creation of html element";
+// // ele.id = "demo"
+// ele.setAttribute("id" , "demo")
+// console.log(ele);
 
-//spread opearator
+// // ele.removeAttribute("id");
+// document.body.appendChild(ele);
 
 
+// let image = document.createElement("img");
+// image.src= "./3606208.jpg";
+// console.log(image);
 
-// let x=[...ele];
-// console.log(x,Array.isArray(x));
-// x.map(element =>{
-//     element.style.backgroundColor="tomato";
-//     element.style.color="white";
-//     element.style.textTransform="uppercase";
+// document.body.appendChild(image)
+
+
+// let form = document.querySelector("form");
+// let username = document.querySelector("#uName");
+// let email = document.querySelector("#uMail");
+// let psw = document.querySelector("#uPsw");
+
+// form.addEventListener("submit",(event)=>{
+//     event.preventDefault();
+//     // console.log(event);
+//     // console.log("form submitted");
+//     // console.log(username.value);
+//     let name =username.value;
+//     let mail = email.value;
+//     let password = psw.value;
+//     let userDetails={
+//         name:name,
+//         mail : mail,
+//         password:password
+//     } ;
+//     // console.log(userDetails);
+//     localStorage.setItem("userData" , JSON.stringify(userDetails))
 // })
- let ele =document.querySelectorAll(".test");
-console.log(ele);
 
-// let ele =document.querySelector("#demo");
-// console.log(ele);
-// let ele=document.querySelector(".test");
-// console.log(ele);
+
+// localStorage.clear()
+
+
+// ! 
+
+// let mainEle = document.createElement("div");
+// mainEle.setAttribute("id" , "mainBlock");
+
+// console.log(mainEle);
+
+
+// let topEle = document.createElement("div");
+// topEle.setAttribute("class" , "topBlock");
+// // console.log(topEle);
+
+// let image = document.createElement("img");
+// image.src="https://cdn.pixabay.com/photo/2023/02/09/16/43/cat-7779237_640.jpg";
+// image.width="300";
+// image.height="300";
+// image.style.objectFit="cover"
+
+// let bottomEle = document.createElement("div");
+// bottomEle.setAttribute("class" , "bottomBlock");
+// // console.log(bottomEle);
+// let h1 = document.createElement("h1");
+// h1.innerText="CAT";
+
+// let btn = document.createElement("button");
+// btn.innerText="View More";
+
+// bottomEle.appendChild(h1);
+// bottomEle.appendChild(btn);
+// topEle.appendChild(image);
+// mainEle.appendChild(topEle);
+// mainEle.appendChild(bottomEle);
+
+// document.body.appendChild(mainEle)
+
+
+// !
+let form = document.querySelector("form");
+let username = document.getElementById("uName");
+let password = document.getElementById("uPass");
+let check=document.getElementById("check");
+let show =document.getElementById("show");
+
+let gender = document.getElementsByName("gender");
+// console.log(gender);
+
+check.addEventListener("click",(event)=>{
+    if(event.target.checked==true){
+        password.setAttribute("type","text");
+        show.innerText="hide password";
+    }else{
+        password.setAttribute("type","password");
+        show.innerText="show password";
+    }
+})
+
+form.addEventListener("submit" , event=>{
+    event.preventDefault();
+    let un = username.value;
+    let up = password.value;
+    let gen = "";
+    for(let i = 0;i<=gender.length-1;i++){
+        // console.log(gender[i].value);
+        // console.log(gender[i].checked);
+        if(gender[i].checked == true){
+            // console.log(gender[i].value);
+            gen += gender[i].value;
+        }
+    };
+    let userDetails={
+        username:un,
+        password:up,
+        gender:gen
+
+    };
+    console.log(userDetails);
+    sessionStorage.setItem("userData" ,JSON.stringify(userDetails) )
+})
